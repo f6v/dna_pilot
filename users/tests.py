ec555763e5df52acf5550b431797483cbd9695d4
f6v <i.am.filippov@gmail.com>
2020-04-06 22:27:38 +0200

    flake8 and some fixes

--- users/tests.py
+++ users/tests.py
@@ -7,7 +7,7 @@ class UsersManagersTests(TestCase):
     def test_create_user(self):
         User = get_user_model()
         user = User.objects.create_user(email='user@test.com',
-            password='testpass123')
+                                        password='testpass123')
         self.assertEqual(user.email, 'user@test.com')
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
